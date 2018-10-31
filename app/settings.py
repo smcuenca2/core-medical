5c1f36815edea5f7c11de830c1f665debe3d87bb
Jorge Luis Malla Sánchez <jomasloja@gmail.com>
2018-10-31 00:49:13 -0500

    Agregar bootstrap y mejorar las urls

--- app/settings.py
+++ app/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'thwxk3g7wu9je1nwvbq4zr8fa_11_m)in%9e-moe7k^6qng7yf'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -119,8 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
--- modules/umls/views.py
+++ modules/umls/views.py
@@ -7,40 +7,39 @@ from pymedtermino.umls import *
 
 
 def index(request):
-    ctx = {'umls_list': []}
-    return render(request, 'index.html', ctx)
+    object_list = []
+    return render(request, 'index.html', locals())
 
 
-def process_codes_from_file(request):
-    DATABASE_CONFIG = getattr(settings, "DATABASES", None)
-    DATABASE_HOST = DATABASE_CONFIG['default']['HOST']
-    DATABASE_USER = DATABASE_CONFIG['default']['USER']
-    DATABASE_PASSWORD = DATABASE_CONFIG['default']['PASSWORD']
-    DATABASE_NAME = DATABASE_CONFIG['default']['NAME']
+def process_umls(request):
+    if request.method == 'POST':
+        DATABASE_CONFIG = getattr(settings, "DATABASES", None)
+        DATABASE_HOST = DATABASE_CONFIG['default']['HOST']
+        DATABASE_USER = DATABASE_CONFIG['default']['USER']
+        DATABASE_PASSWORD = DATABASE_CONFIG['default']['PASSWORD']
+        DATABASE_NAME = DATABASE_CONFIG['default']['NAME']
 
-    connect_to_umls_db(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD,
-                       DATABASE_NAME, encoding="latin1")
+        connect_to_umls_db(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD,
+                           DATABASE_NAME, encoding="latin1")
+        list_umls_cui = []
+        with open('codes_umls.csv', mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=';')
 
-    list_umls_cui = []
+            for row in csv_reader:
+                umls_cui = UMLS_CUI(row['CUI'])
+                list_umls_cui.append(umls_cui)
 
-    with open('codes_umls.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=';')
+        object_list = []
+        for umls_cui in list_umls_cui:
+            if 'may_be_treated_by' in umls_cui.relations:
+                for relations in umls_cui.may_be_treated_by:
+                    sicknessTreatDTO = SicknessTreatDTO()
+                    sicknessTreatDTO.code = umls_cui.code
+                    sicknessTreatDTO.term = umls_cui.term
+                    sicknessTreatDTO.original_terminologies = umls_cui.original_terminologies
+                    sicknessTreatDTO.term_rel = relations.term
+                    sicknessTreatDTO.terminology = umls_cui.terminology.name
+                    sicknessTreatDTO.code_rel = relations.code
+                    object_list.append(sicknessTreatDTO)
 
-        for row in csv_reader:
-            umls_cui = UMLS_CUI(row['CUI'])
-            list_umls_cui.append(umls_cui)
-
-    result = []
-    for umls_cui in list_umls_cui:
-        if 'may_be_treated_by' in umls_cui.relations:
-            for relations in umls_cui.may_be_treated_by:
-                sicknessTreatDTO = SicknessTreatDTO()
-                sicknessTreatDTO.code = umls_cui.code
-                sicknessTreatDTO.term = umls_cui.term
-                sicknessTreatDTO.original_terminologies = umls_cui.original_terminologies
-                sicknessTreatDTO.term_rel = relations.term
-                sicknessTreatDTO.terminology = umls_cui.terminology.name
-                sicknessTreatDTO.code_rel = umls_cui.code
-                result.append(sicknessTreatDTO)
-
-    return render(request, 'index.html', {'umls_list': result})
+    return render(request, 'index.html', locals())
