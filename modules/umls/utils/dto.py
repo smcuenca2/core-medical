441f77a516db6b94f22b4281a3572e90cde3ab5c
Jorge Luis Malla Sánchez <jomasloja@gmail.com>
2018-11-06 00:20:45 -0500

    Renombrar nombres de clases Data Transfer Object.

--- modules/umls/utils/dto.py
+++ modules/umls/utils/dto.py
@@ -1,10 +1,20 @@
-class SicknessTreatDTO():
+class Concept():
     code = None
     term = None
     terminology = None
     original_terminologies = None
-    term_rel = None
-    code_rel = None
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+class Relation(Concept):
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+class ConceptDTO(Concept):
+    relation = Relation
 
     def __repr__(self):
         return str(self.__dict__)
--- modules/umls/views.py
+++ modules/umls/views.py
@@ -3,7 +3,7 @@ import csv
 from django.conf import settings
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from modules.umls.utils.dto import SicknessTreatDTO
+from modules.umls.utils.dto import Relation, ConceptDTO
 from pymedtermino.umls import *
 
 
@@ -27,32 +27,33 @@ def process_umls(request):
                                      None)
     connect_to_umls()
     list_umls_cui = []
+    concepts = []
 
-    with open('codes_umls.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=';')
-
-        for row in csv_reader:
-            umls_cui = UMLS_CUI(row['CUI'])
-            list_umls_cui.append(umls_cui)
-
-    sicknessTreatList = []
     if search is not None and search != "":
-        list_umls_cui = list(
-            filter(lambda umls: umls.code == search, list_umls_cui))
+        umls_cui = UMLS_CUI(search)
+        list_umls_cui.append(umls_cui)
+    else:
+        with open('codes_umls.csv', mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=';')
+
+            for row in csv_reader:
+                umls_cui = UMLS_CUI(row['CUI'])
+                list_umls_cui.append(umls_cui)
 
     for umls_cui in list_umls_cui:
         if relation_selected in umls_cui.relations:
             for relations in getattr(umls_cui, relation_selected):
-                sicknessTreatDTO = SicknessTreatDTO()
-                sicknessTreatDTO.code = umls_cui.code
-                sicknessTreatDTO.term = umls_cui.term
-                sicknessTreatDTO.original_terminologies = umls_cui.original_terminologies
-                sicknessTreatDTO.term_rel = relations.term
-                sicknessTreatDTO.terminology = umls_cui.terminology.name
-                sicknessTreatDTO.code_rel = relations.code
-                sicknessTreatList.append(sicknessTreatDTO)
+                concept = ConceptDTO()
+                concept.relation = Relation()
+                concept.code = umls_cui.code
+                concept.term = umls_cui.term
+                concept.original_terminologies = umls_cui.original_terminologies
+                concept.relation.term = relations.term
+                concept.terminology = umls_cui.terminology.name
+                concept.relation.code = relations.code
+                concepts.append(concept)
 
-    paginator = Paginator(sicknessTreatList, PAGINATOR_NUMBER_ITEMS)
+    paginator = Paginator(concepts, PAGINATOR_NUMBER_ITEMS)
     page = request.GET.get('page')
     object_list = paginator.get_page(page)
     return render(request, 'index.html', locals())
